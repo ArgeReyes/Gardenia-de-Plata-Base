30457585dd5f59510fe20ba00c40884c1f390ba9
ArgeReyes <44539912+ArgeReyes@users.noreply.github.com>
2022-05-05 23:50:33 -0600

    Cambios Marti

--- Proyecto/src/proyecto/cifrar.java
+++ Proyecto/src/proyecto/cifrar.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package proyecto;
+import javax.crypto.*;
+import java.security.*;
 
 /**
  *
@@ -10,4 +12,22 @@ package proyecto;
  */
 public class cifrar {
     
-}
+    
+        public String cifrado (String cadena)
+    {
+        try{
+            java.security.MessageDigest cf = java.security.MessageDigest.getInstance("MD5");
+            byte[] array = cf.digest(cadena.getBytes());
+            StringBuffer sb = new StringBuffer();
+            for (int i=0; i<array.length; i++){
+            
+                sb.append(Integer.toHexString((array[i] & 0xFF) |0X100).substring(1,3));
+            }
+                
+        return sb.toString();
+    }catch (java.security.NoSuchAlgorithmException e){
+        
+    
+    }
+    
+
